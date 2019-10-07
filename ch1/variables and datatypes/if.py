f70b58c30ad10252d62952a95e4b06935b1abe53
apbjnr <andries@r00t.co.za>
2019-10-07 15:17:30 +0200

    conditional_statements

---
+++ ch1/variables and datatypes/if.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+name = raw_input("what is your name? ")
+
+print "your name is " + name
+
+if name == "apb":
+	print "you are apb"
+	print "the sysadmin"
+
+elif name == "sigh":
+	print "you are sigh"
+	print "a n00b"
+else:
+	print "unknown user"
---
+++ ch1/variables and datatypes/while.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+age = 20
+
+while age > 10:
+
+	age = int(raw_input("What is your age? "))
+
+	if age > 10 :	
+		print "your age is > 10"
+	else : 
+		print "your age is <=10"
+
