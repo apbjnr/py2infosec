7679ef991985cab24799e36f8aaed092a64ea393
apbjnr <andries@r00t.co.za>
2020-01-08 10:28:55 +0200

    creating packages

---
+++ ch1/variables and datatypes/pack.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import packagedemo
+
+im = packagedemo.Calculator(10,20)
+
+# add the above specified values (10,20) 
+print '%d'%im.add()
---
+++ ch1/variables and datatypes/packagedemo/__init__.py
@@ -0,0 +1,1 @@
+from modules import Calculator, Scientific
